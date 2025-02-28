e0d92fd3d642693bcf457d90e2c79b63696e7f1c
Adam Hlavacek <git@adamhlavacek.com>
2025-02-28 18:01:39 +0100

    feat: always replace whole directory

--- build.sh
+++ build.sh
@@ -17,18 +17,22 @@ fi
 if [ -d public ];
     then cp -r public/* .
 fi
-node util-games-builder/build-game.js game.html
+node util-games-builder/build-game.js game.html --too-large-ok
 rm -r dist/aux
 GAME_ID="$(jq .id dist/game.html.manifest.json | sed -E 's/^"(.*)"$/\1/')"
+if [ -z "$GAME_ID" ];
+    then echo "ERROR: Game ID not found in manifest"; exit 1
+fi
 
 git config --global user.email "git@qrgamestudio.com"
 git config --global user.name "QR Bot"
 
 git clone git@github.com:QRGameStudio/repo-games.git
 DIR_GAME="repo-games/$GAME_ID"
-if [ ! -d "$DIR_GAME" ]; then
-    mkdir "$DIR_GAME"
+if [ -d "$DIR_GAME" ]; then
+  rm -r "$DIR_GAME"
 fi
+mkdir "$DIR_GAME"
 cp dist/* "$DIR_GAME"
 cd repo-games
 git add "$GAME_ID"
